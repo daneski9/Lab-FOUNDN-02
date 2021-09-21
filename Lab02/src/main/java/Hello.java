7fadda339179644aee8c876d7632210f64783cfb
Christian Long <ChrisLong561@gmail.com>
2021-09-20 19:01:33 -0700

    Update Hello.java

--- Lab02/src/main/java/Hello.java
+++ Lab02/src/main/java/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301719561 - This line was added by Dane Coleman.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219256207 - This line was added by Chris Long.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
